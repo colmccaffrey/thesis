fa9627c6bf42d2a3eefd8e647df09196a526e9b3
Colleen McCaffrey <colleenmccaffrey@Colleens-MacBook-Pro.local>
2019-05-09 11:42:05 -0400

    fixed content

--- src/js/plot-charts.js
+++ src/js/plot-charts.js
@@ -2,7 +2,7 @@
 // set the dimensions and margins of the graph
 var margin = {top: 30, right: 30, bottom: 70, left: 60},
     width = 600 - margin.left - margin.right,
-    height = 400 - margin.top - margin.bottom;
+    height = 450 - margin.top - margin.bottom;
 
 // append the svg object to the body of the page
 var svg = d3.select("#my_dataviz")
